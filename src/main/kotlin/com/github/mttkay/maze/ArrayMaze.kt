8454c3e4482dc297131dd81c1f057b0fe3acfbb4
Matthias Kaeppler <m.kaeppler@gmail.com>
2017-04-09 21:02:33 +0200

    Make ArrayMaze iterable, back by IntArray for less allocations

--- src/main/kotlin/com/github/mttkay/maze/ArrayMaze.kt
+++ src/main/kotlin/com/github/mttkay/maze/ArrayMaze.kt
@@ -1,27 +1,63 @@
 package com.github.mttkay.maze
 
 /**
- * A [Maze] implementation backed by a two-dimensional [Array].
+ * A [Maze] implementation backed by a two-dimensional [Array]. Each row is
+ * backed by an [IntArray] for efficient tile representation. Iteration happens
+ * in row-major order.
  */
-class ArrayMaze(override val width: Int, override val height: Int) : Maze {
+class ArrayMaze(override val width: Int, override val height: Int) : Maze, Iterable<Tile> {
 
-  // models a rectangular stage in column major order
-  private val tiles: Array<Array<Tile>> = Array(width) { Array(height) { Tile.SOLID } }
+  private val Int.asTile: Tile get() = Tile.values().find { it.ordinal == this }!!
+
+  init {
+    require(width > 0 && height > 0) {
+      "Must be at least 1x1 in size"
+    }
+  }
+
+  override fun iterator(): Iterator<Tile> = object : Iterator<Tile> {
+    private val rowIter = tiles.iterator()
+    private var colIter = rowIter.next().iterator()
+
+    override fun hasNext(): Boolean {
+      val hasMoreColumns = colIter.hasNext()
+      return if (!hasMoreColumns) {
+        rowIter.hasNext()
+      } else hasMoreColumns
+    }
+
+    override fun next(): Tile {
+      when {
+        colIter.hasNext() ->
+          // proceed reading from current row
+          return colIter.next().asTile
+        rowIter.hasNext() -> {
+          // there are more rows; advance to next row
+          colIter = rowIter.next().iterator()
+          return colIter.next().asTile
+        }
+        else -> throw NoSuchElementException("No more tiles to read")
+      }
+    }
+  }
+
+  // models a rectangular stage in row major order
+  private val tiles: Array<IntArray> = Array(height) { IntArray(width) { Tile.SOLID.ordinal } }
 
   override fun getTile(x: Int, y: Int): Tile {
     require(x < width) { "x ($x) must be smaller than width ($width)" }
     require(y < height) { "y ($y) must be smaller than height ($height)" }
-    return tiles[x][y]
+    return tiles[y][x].asTile
   }
 
   override fun setTile(x: Int, y: Int, tile: Tile) {
     require(x < width) { "x ($x) must be smaller than width ($width)" }
     require(y < height) { "y ($y) must be smaller than height ($height)" }
-    tiles[x][y] = tile
+    tiles[y][x] = tile.ordinal
   }
 
   override fun fill(tile: Tile) {
-    tiles.forEach { column -> column.forEach { _ -> column.fill(tile) } }
+    tiles.forEach { column -> column.forEach { _ -> column.fill(tile.ordinal) } }
   }
 
 }
